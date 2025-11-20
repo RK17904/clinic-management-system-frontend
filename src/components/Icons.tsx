e7528e2421810a9c393141ea6c424e4601a236ef
Ravindu Kaveesha <ravindu.kaveesha2001@gmail.com>
2025-11-21 03:09:38 +0530

    add icons for admin controls

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -44,3 +44,22 @@ DE   <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
     <line x1="17" y1="11" x2="23" y2="11"></line>
   </svg>
 );
+
+/* --- Admin control icons-- */
+export const PlusIcon = () => ( //add icon
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <line x1="12" y1="5" x2="12" y2="19"></line>
+    <line x1="5" y1="12" x2="19" y2="12"></line>
+  </svg>
+);
+
+export const ListIcon = () => ( //list view icon
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <line x1="8" y1="6" x2="21" y2="6"></line>
+    <line x1="8" y1="12" x2="21" y2="12"></line>
+    <line x1="8" y1="18" x2="21" y2="18"></line>
+    <line x1="3" y1="6" x2="3.01" y2="6"></line>
+    <line x1="3" y1="12" x2="3.01" y2="12"></line>
+    <line x1="3" y1="18" x2="3.01" y2="18"></line>
+  </svg>
+);
